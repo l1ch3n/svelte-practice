5b4d8e3a25565723efae257dbb79c6250a9deb70
Nicole <nicole.shukin@gmail.com>
2023-11-02 19:01:40 -0600

    added component

---
+++ svelte-practice/src/lib/components/card.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let title;
+  export let description;
+  export let buttonText;
+</script>
+
+<article class="m-2 bg-red-50 w-48">
+  <div class=" my-2 bg-black text-w"></div>
+  <h3>{title}</h3>
+  <p>{description}</p>
+  <button>{buttonText}</button>
+</article>
